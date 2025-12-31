c460093d3c94c5c2f4bba766bd349b9b6e8bd06f
Mavericks <mavericks.devs.tech@gmail.com>
2025-12-31 21:39:16 +0530

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve("public");
+  const distPath = path.resolve("client/dist");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
